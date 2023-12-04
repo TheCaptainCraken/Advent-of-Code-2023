1324edc17a4c57b8e19e5c2306a8260ad2b39d53
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2023-12-04 15:59:41 +0100

    added day 2 haskell

--- day_2/haskell/FirstPart/Solution.hs
+++ day_2/haskell/FirstPart/Solution.hs
@@ -1,4 +1,29 @@
 module FirstPart.Solution(solve) where
 
+import Data.Maybe(isJust)
+
 solve:: String -> String
-solve =  "TODO"
+solve input = show . sumIds . handleGames . parseInput $ input
+
+data Game = Game { red::Int, green::Int, blue::Int }
+
+sumIds :: [Bool] -> Int
+sumIds ids = foldr (\(i, id) sum -> if id then sum+i else sum) 0 $ zip [1..] ids
+
+startingGame = Game 0 0 0
+
+parseInput input = map (words . (filter(`notElem` ",;"))) (lines input)
+
+handleGames::[[String]] -> [Bool]
+handleGames games = map (isJust . (\x -> handleGame x (Just startingGame)) . drop 2) $ games where
+
+handleGame:: [String] -> Maybe Game -> Maybe Game
+handleGame _ Nothing = Nothing
+handleGame (v:"red":rest) (Just (Game red green blue)) = if (parseInt v) > 12 then Nothing else handleGame rest (Just (Game (red + parseInt v) green blue))
+handleGame (v:"green":rest) (Just (Game red green blue)) = if (parseInt v) > 13 then Nothing else handleGame rest (Just (Game red (green + parseInt v) blue))
+handleGame (v:"blue":rest) (Just (Game red green blue)) = if (parseInt v) > 14 then Nothing else handleGame rest (Just (Game red green (blue + parseInt v)))
+handleGame [] game = game
+handleGame _ _ = Nothing -- this is never reached because of guarantees from the problem authors
+
+parseInt :: String -> Int
+parseInt n = read n
--- day_2/haskell/SecondPart/Solution.hs
+++ day_2/haskell/SecondPart/Solution.hs
@@ -1,4 +1,26 @@
 module SecondPart.Solution(solve) where
 
+import Data.Maybe(isJust)
+
 solve:: String -> String
-solve = "TODO"
+solve input = show . sum . handleGames . parseInput $ input
+
+data Game = Game { red::Int, green::Int, blue::Int }
+
+startingGame = Game 0 0 0
+
+parseInput input = map (words . (filter(`notElem` ",;"))) (lines input)
+
+handleGames::[[String]] -> [Int]
+handleGames games = map ( calculateGame . (\x -> handleGame x startingGame) . drop 2) $ games where
+    calculateGame (Game red blue green) = red * blue * green
+
+handleGame:: [String] -> Game -> Game
+handleGame (v:"red":rest) (Game red green blue) = let new = parseInt v in handleGame rest (Game (if new > red then new else red) green blue)
+handleGame (v:"green":rest) (Game red green blue) = let new = parseInt v in handleGame rest (Game red (if new > green then new else green) blue)
+handleGame (v:"blue":rest) (Game red green blue) = let new = parseInt v in handleGame rest (Game red green (if new > blue then new else blue))
+handleGame [] game = game
+-- this is never reached because of guarantees from the problem authors
+
+parseInt :: String -> Int
+parseInt n = read n
